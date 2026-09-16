9de62f3b0029cbca7716f08ca9eaaf7b0a5b024f
NightEpiphany <nightepiphany0401@gmail.com>
2026-09-17 01:07:29 +0800

    配方物品渲染角度纠正

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/client/render/block/RecipeBlockEntityRender.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/client/render/block/RecipeBlockEntityRender.java
@@ -70,6 +70,7 @@ public class RecipeBlockEntityRender implements BlockEntityRenderer<RecipeBlockE
             poseStack.translate(1, 1.25, 0);
         } else {
             poseStack.translate(1, 0.75, 2);
+            poseStack.mulPose(Axis.ZP.rotationDegrees(180F));
         }
 
         blockEntityRenderState.targetItem.submit(poseStack, submitNodeCollector, blockEntityRenderState.lightCoords, OverlayTexture.NO_OVERLAY, 0);
